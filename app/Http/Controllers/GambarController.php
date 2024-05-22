fdd260ff6b91b46f0f375100df710e08479bc8f7
aistaqiemsy <a.istakim@gmail.com>
2024-05-23 06:25:45 +0700

    Bisa unggah gambar lokal

---
+++ app/Http/Controllers/GambarController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Storage;
+
+class GambarController extends Controller
+{
+    public function simpanGambar(Request $request) {
+        if($request->hasFile('gambar')) {
+            // membuat (menciptakan folder 'path_ke_folder') bila belum ada
+            // jalankan perintah : php artisan storage:link 
+            // sebelum menjalankan aplikasi laravel dan akan membuat folder 'path_ke_folder' di dalam /public/storage
+            $folder = 'path_ke_folder'; 
+            $fileName = $request->file('gambar')->hashName();
+            $path = Storage::disk('public')->putFileAs($folder, $request->file('gambar'), $fileName);
+
+            return "Gambar berhasil di unggah! <br>Lokasi penyimpanan $path dengan nama file $fileName";
+        }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\GambarController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/gambar/simpanGambar', [GambarController::class, 'simpanGambar']);
