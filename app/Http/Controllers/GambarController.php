9f2a7609065de694a74a180dbceacb35feafa107
aistaqiemsy <a.istakim@gmail.com>
2024-05-23 09:25:24 +0700

    Bisa unggah gambar ke db dan tampil gambar

--- app/Http/Controllers/GambarController.php
+++ app/Http/Controllers/GambarController.php
@@ -4,23 +4,39 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Storage;
+use Illuminate\Support\Facades\DB;
 
 class GambarController extends Controller
 {
+    // public function simpanGambar(Request $request) {
+    //     if($request->hasFile('gambar')) {
+    //         // membuat (menciptakan folder 'path_ke_folder') bila belum ada
+    //         // jalankan perintah : php artisan storage:link 
+    //         // sebelum menjalankan aplikasi laravel dan akan membuat folder 'path_ke_folder' di dalam /public/storage
+    //         $folder = 'path_ke_folder'; 
+    //         $fileName = $request->file('gambar')->hashName();
+    //         $path = Storage::disk('public')->putFileAs($folder, $request->file('gambar'), $fileName);
+
+    //         return "Gambar berhasil di unggah! <br>Lokasi penyimpanan <b>$path</b><br> Dengan nama file <b>$fileName</b><hr><center><a href='/gambar/tampil'>Tampilkan Gambar</a></center>";
+    //     }
+    // }
+
     public function simpanGambar(Request $request) {
         if($request->hasFile('gambar')) {
-            // membuat (menciptakan folder 'path_ke_folder') bila belum ada
-            // jalankan perintah : php artisan storage:link 
-            // sebelum menjalankan aplikasi laravel dan akan membuat folder 'path_ke_folder' di dalam /public/storage
-            $folder = 'path_ke_folder'; 
+            $folder = 'path_ke_folder';
             $fileName = $request->file('gambar')->hashName();
             $path = Storage::disk('public')->putFileAs($folder, $request->file('gambar'), $fileName);
 
-            return "Gambar berhasil di unggah! <br>Lokasi penyimpanan <b>$path</b><br> Dengan nama file <b>$fileName</b><hr><center><a href='/gambar/tampil'>Tampilkan Gambar</a></center>";
+            DB::table('tb_gambar')->insert([
+                'nama_gambar' => $fileName
+            ]);
+
+            return "Gambar <b>$fileName</b> berhasil di unggah ke MySQL<hr>";
         }
     }
 
     public function tampilGambar() {
-        return view('tampil');
+        $gambar = DB::table('tb_gambar')->get();
+        return view('tampil', ['gambar' => $gambar]);
     }
 }
--- resources/views/tampil.blade.php
+++ resources/views/tampil.blade.php
@@ -1,3 +1,8 @@
-<center>
+
+<!-- <center>
     <img src="{{ URL::to('/') }}/storage/path_ke_folder/MKODD3IAk4oVnV4yZkM2YAZEUQeNu28gsCxwYHBZ.jpg" width="250" height="250">
-</center>
+</center> -->
+
+@foreach ( $gambar as $gambardb )
+    <img src="{{ URL::to('/') }}/storage/path_ke_folder/{{ $gambardb->nama_gambar }}" width="100" height="100">
+@endforeach
